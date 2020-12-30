b290d9c9fc1efe56a01b41ae9b700363e0a9b127
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-12-30 17:47:47 +0600

    general project update commit

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,30 +1,28 @@
-CREATE DATABASE  my_db;
+CREATE DATABASE my_db;
 USE my_db;
 
-CREATE TABLE users (
-  username varchar(15),
-  password varchar(100),
-  enabled tinyint(1),
-  PRIMARY KEY (username)
-) ;
+CREATE TABLE users
+(
+    username varchar(15),
+    password varchar(100),
+    enabled  tinyint(1),
+    PRIMARY KEY (username)
+);
 
-CREATE TABLE authorities (
-  username varchar(15),
-  authority varchar(25),
-  FOREIGN KEY (username) references users(username)
-) ;
+CREATE TABLE authorities
+(
+    username  varchar(15),
+    authority varchar(25),
+    FOREIGN KEY (username) references users (username)
+);
 
 INSERT INTO my_db.users (username, password, enabled)
-VALUES
-	('Adilkhan', '{noop}adilkhan', 1),
-	('Elena', '{noop}elena', 1),
-	('Ivan', '{noop}ivan', 1);
-    
+VALUES ('adilkhan', '{noop}adilkhan', 1),
+       ('elena', '{noop}elena', 1),
+       ('ivan', '{noop}ivan', 1);
+
 INSERT INTO my_db.authorities (username, authority)
-VALUES
-	('Adilkhan', 'ROLE_EMPLOYEE'),
-	('Elena', 'ROLE_HR'),
-    ('Ivan', 'ROLE_HR'),
-	('Ivan', 'ROLE_MANAGER');
-    
-    
+VALUES ('adilkhan', 'ROLE_EMPLOYEE'),
+       ('elena', 'ROLE_HR'),
+       ('ivan', 'ROLE_HR'),
+       ('ivan', 'ROLE_MANAGER');
